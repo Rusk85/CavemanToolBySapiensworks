6b55a905dac8c1a22f47ebd012e6d41aa456112b
MikeSW <MikeSW@localhost>
2011-12-09 15:24:02 +0200

    fixed bug in UserRightModule added CavemanUserContext filter

--- src/CavemanTools.MVC/Attributes/MvcRequestMetrics.cs
+++ src/CavemanTools.MVC/Attributes/MvcRequestMetrics.cs
@@ -6,24 +6,23 @@ namespace CavemanTools.Mvc.Attributes
 {
     /// <summary>
     /// RequestStatsModule is required to be active, in order to show the metrics
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class)]
-    public class TimeMvcRequestAttribute:ActionFilterAttribute
+    /// </summary>    
+    public class MvcRequestMetrics:IActionFilter,IResultFilter
     {
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        public void OnActionExecuting(ActionExecutingContext filterContext)
         {
             RequestStatsModule.HandleDuration(RequestStatsModule.MvcActionDuration);            
         }
-        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        public void OnActionExecuted(ActionExecutedContext filterContext)
         {
             RequestStatsModule.HandleDuration(RequestStatsModule.MvcActionDuration);            
         }
 
-        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        public void OnResultExecuting(ResultExecutingContext filterContext)
         {
             RequestStatsModule.HandleDuration(RequestStatsModule.MvcResultDuration);            
         }
-        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        public void OnResultExecuted(ResultExecutedContext filterContext)
         {
             RequestStatsModule.HandleDuration(RequestStatsModule.MvcResultDuration);            
         }
--- src/CavemanTools.MVC/Security/UserRightModule.cs
+++ src/CavemanTools.MVC/Security/UserRightModule.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Security.Principal;
 using System.Web;
+using System.Web.Mvc;
 using System.Web.Security;
 using CavemanTools.Web.Security;
+using CavemanTools.Web;
 
 namespace CavemanTools.Mvc.Security
 {
     /// <summary>
-    /// Use this with the HttpModuleMagic.Mvc3 nuget package for dependecy injection
+    /// Use this with the HttpModuleMagic.Mvc3 nuget package for dependecy injection.
+    /// Automatically creates UserContext
     /// </summary>
     public class UserRightModule:IHttpModule
     {
-        private IUserRightsRepository _repo;
+        private Func<IUserRightsRepository> _repo;
 
-        public UserRightModule(IUserRightsRepository repository)
+        public UserRightModule(Func<IUserRightsRepository> repository)
         {
             if (repository == null) throw new ArgumentNullException("repository");
             _repo = repository;
@@ -26,31 +29,48 @@ namespace CavemanTools.Mvc.Security
         void context_PostAuthorizeRequest(object sender, System.EventArgs e)
         {
             var ctx = (sender as HttpApplication).Context;
-            ctx.Items["_usr-context_"]=(ctx.User.Identity.IsAuthenticated)?GetUserContext(ctx.User.Identity):GetAnonymousContext();        
+            if (ctx.Request.MatchesStaticResource()) return;
+            CreateUserRightContext(ctx,_repo());
         }
 
-        private IUserRightsContext GetUserContext(IIdentity identity)
+        internal static void CreateUserRightContext(HttpContext ctx,IUserRightsRepository repo)
+        {
+            ctx.Items["_usr-context_"] = (ctx.User.Identity.IsAuthenticated) ? GetUserContext(ctx.User.Identity, repo) : GetAnonymousContext(repo);        
+        }
+
+        internal static IUserRightsContext GetUserContext(IIdentity identity,IUserRightsRepository repo)
         {
             var fi = identity as FormsIdentity;
             if (fi == null) throw new NotSupportedException("Only FormsAuthentication is supported");
             var authData = fi.Ticket.UserData.Unpack();
-            if (authData == null) return GetAnonymousContext();
+            if (authData == null) return GetAnonymousContext(repo);
             
             //get group
-            var grp = _repo.GetGroupById(authData.GroupId);
+            var grp = repo.GetGroupById(authData.GroupId);
             var usr = new UserRightsContext(authData.Id,grp);
             usr.Name = fi.Name;
             return usr;
         }
 
-        IUserRightsContext GetAnonymousContext()
+        internal static IUserRightsContext GetAnonymousContext(IUserRightsRepository repo)
         {
-           return new UserRightsContext(null,_repo.GetDefaultGroup());
+           return new UserRightsContext(null,repo.GetDefaultGroup());
         }
 
         public void Dispose()
         {
             
         }
+    }
+
+    /// <summary>
+    /// Asp.net mvc 3 global filter which creates UserContext
+    /// </summary>
+    public sealed class CavemanUserContext:IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            UserRightModule.CreateUserRightContext(HttpContext.Current, DependencyResolver.Current.GetService<IUserRightsRepository>());
+        }
     }
 }
