0c7b6d0481726408a964b452aa4bd7512a862095
MikeSW <MikeSW@localhost>
2011-12-10 13:30:10 +0200

    tests for user credential validation

---
+++ src/XTests/Security/UserRightsContextTests.cs
@@ -0,0 +1,35 @@
+﻿using CavemanTools.Web.Security;
+using Xunit;
+using System;
+using System.Diagnostics;
+
+namespace XTests.Security
+{
+    public class UserRightsContextTests
+    {
+        private Stopwatch _t = new Stopwatch();
+        private UserRightsContext _user;
+
+        public UserRightsContextTests()
+        {
+            _user = new UserRightsContext(1, new UserContextGroup(1, new byte[1] {UserBasicRights.Login}));            
+        }
+
+        [Fact]
+        public void user_is_member_of_a_group()
+        {
+            Assert.True(_user.IsMemberOf(new int[]{1,2}));
+        }
+
+
+        [Fact]
+        public void user_has_right_to_login()
+        {
+            Assert.True(_user.HasRightTo(UserBasicRights.Login));
+        }
+        private void Write(string format, params object[] param)
+        {
+            Console.WriteLine(format, param);
+        }
+    }
+}
