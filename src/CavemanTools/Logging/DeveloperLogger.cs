59e3451f3a45b744d2b156117586a298382562d1
MikeSW <MikeSW@localhost>
2012-02-20 21:12:32 +0200

    changed  DeveloperLogger to actually work

--- src/CavemanTools/Logging/DeveloperLogger.cs
+++ src/CavemanTools/Logging/DeveloperLogger.cs
@@ -1,20 +1,30 @@
-﻿namespace CavemanTools.Logging
+﻿using System;
+
+namespace CavemanTools.Logging
 {
     public class DeveloperLogger:LogWriterBase
     {
+        private Action<string> _writer;
+
         public override T GetLogger<T>()
         {
             throw new System.NotImplementedException();
         }
 
+        public DeveloperLogger(Action<string> writer)
+        {
+            _writer = writer;
+        }
+
         public override void Log(LogLevel level, string text)
         {
-            System.Diagnostics.Debug.WriteLine(text,level.ToString());
+            _writer(DateTime.Now.ToString() + " - " + level.ToString() + ": " + text);
+
         }
 
         public override void Log(LogLevel level, string message, params object[] args)
         {
-            System.Diagnostics.Debug.WriteLine(string.Format(message,args), level.ToString());
+            _writer(DateTime.Now.ToString() + " - " + level.ToString() + ": " + string.Format(message,args));            
         }
     }
 }
