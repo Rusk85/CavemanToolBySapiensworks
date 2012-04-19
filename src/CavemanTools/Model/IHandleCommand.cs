e579534e0f9ce2e318f18b9fefbbcd4dc22cc08d
MikeSW <MikeSW@localhost>
2012-04-19 18:00:15 +0300

    added interface for handling Commands and Requests (Commands with results)

--- src/CavemanTools/Model/IHandleCommand.cs
+++ src/CavemanTools/Model/IHandleCommand.cs
@@ -3,9 +3,31 @@ namespace CavemanTools.Model
     /// <summary>
     /// Interface for command handlers
     /// </summary>
-    /// <typeparam name="T">A reference type</typeparam>
-    public interface IHandleCommand<T> where T:class
+    /// <typeparam name="TCommand">A reference type</typeparam>
+    public interface IHandleCommand<TCommand> where TCommand:class
     {
-        void Handle(T command);
+        /// <summary>
+        /// Handle command
+        /// <exception cref="BusinessRuleException"></exception>
+        /// </summary>
+        /// <param name="command"></param>
+        void Handle(TCommand command);
     }
+
+    /// <summary>
+    /// Interface for command handler when you also need to return a response to the user
+    /// </summary>
+    /// <typeparam name="TCommand">reference type</typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    public interface IHandleRequest<in TCommand, out TResult> where TCommand:class 
+    {
+        /// <summary>
+        /// Handle command 
+        /// <exception cref="BusinessRuleException"></exception>
+        /// </summary>
+        /// <returns></returns>
+        TResult  Process(TCommand command);
+    }
+
+   
 }
