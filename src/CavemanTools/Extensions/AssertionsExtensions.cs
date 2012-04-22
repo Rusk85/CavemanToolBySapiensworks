72fd2431f2bf21deee0260546a0e82bf16f42d91
MikeSW <MikeSW@localhost>
2012-04-22 18:42:44 +0300

    added basic code assertions

---
+++ src/CavemanTools/Extensions/AssertionsExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace System
+{
+    public static class AssertionsExtensions
+    {
+         public static void ShouldNotBeNull<T>(this T param,string paramName=null) where T:class
+         {
+             if (param == null) throw new ArgumentNullException(paramName??string.Empty);
+         }
+
+        public static void ShouldNotBeEmpty(this string arg,string paramName=null)
+        {
+            if (string.IsNullOrWhiteSpace(arg)) throw new FormatException(string.Format("Argument '{0}' must not be null, empty or whitespaces",paramName??""));
+        }
+    }
+}
