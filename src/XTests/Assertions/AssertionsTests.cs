fd83b1320ff93b4bc32000c46f7abfe426f77e9c
MikeSW <MikeSW@localhost>
2012-04-22 18:42:44 +0300

    added basic code assertions

---
+++ src/XTests/Assertions/AssertionsTests.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using System;
+using System.Diagnostics;
+
+namespace XTests.Assertions
+{
+    public class AssertionsTests
+    {
+        private Stopwatch _t = new Stopwatch();
+
+        public AssertionsTests()
+        {
+
+        }
+
+        [Fact]
+        public void require_null_throws_when_arg_is_null()
+        {
+            string f = null;
+            Assert.Throws<ArgumentNullException>(() => f.ShouldNotBeNull("f"));            
+        }
+
+        [Fact]
+        public void require_null_doesnt_throw_when_arg_is_not_null()
+        {
+            var f = "hey";
+            Assert.DoesNotThrow(()=>f.ShouldNotBeNull());
+        }
+
+        [Fact]
+        public void require_not_empty_throws_on_empty_or_whitespace_string()
+        {
+            var f = "     ";
+            Assert.Throws<FormatException>(() => f.ShouldNotBeEmpty());
+
+            Assert.DoesNotThrow(()=>"hey".ShouldNotBeEmpty());
+        }
+
+        private void Write(string format, params object[] param)
+        {
+            Console.WriteLine(format, param);
+        }
+    }
+}
