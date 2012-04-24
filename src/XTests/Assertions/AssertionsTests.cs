308072a72d248c75abe9971238f0c5dea17ed072
MikeSW <MikeSW@localhost>
2012-04-24 18:00:15 +0300

    renamed Assertions

--- src/XTests/Assertions/AssertionsTests.cs
+++ src/XTests/Assertions/AssertionsTests.cs
@@ -17,23 +17,23 @@ namespace XTests.Assertions
         public void require_null_throws_when_arg_is_null()
         {
             string f = null;
-            Assert.Throws<ArgumentNullException>(() => f.ShouldNotBeNull("f"));            
+            Assert.Throws<ArgumentNullException>(() => f.MustNotBeNull("f"));            
         }
 
         [Fact]
         public void require_null_doesnt_throw_when_arg_is_not_null()
         {
             var f = "hey";
-            Assert.DoesNotThrow(()=>f.ShouldNotBeNull());
+            Assert.DoesNotThrow(()=>f.MustNotBeNull());
         }
 
         [Fact]
         public void require_not_empty_throws_on_empty_or_whitespace_string()
         {
             var f = "     ";
-            Assert.Throws<FormatException>(() => f.ShouldNotBeEmpty());
+            Assert.Throws<FormatException>(() => f.MustNotBeEmpty());
 
-            Assert.DoesNotThrow(()=>"hey".ShouldNotBeEmpty());
+            Assert.DoesNotThrow(()=>"hey".MustNotBeEmpty());
         }
 
         private void Write(string format, params object[] param)
