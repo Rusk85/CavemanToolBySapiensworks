bf4068cbc69cea10ff7f5ade48e5a1e2a9b6d593
MikeSW <MikeSW@localhost>
2012-04-26 16:03:29 +0300

    added value objects support

---
+++ src/CavemanTools/Model/ValueObjects/Email.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace CavemanTools.Model.ValueObjects
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Email:AbstractValueObject<string>,IEquatable<Email>
+    {
+        
+        public Email(string value) : base(value)
+        {
+            
+        }
+
+        protected override bool Validate(string value)
+        {
+            return IsValid(value);
+        }
+
+        public static bool IsValid(string value)
+        {
+            return value.IsEmail();
+        }
+
+        public bool Equals(Email other)
+        {
+            return other != null && other.Value == Value;
+        }
+    }
+}
