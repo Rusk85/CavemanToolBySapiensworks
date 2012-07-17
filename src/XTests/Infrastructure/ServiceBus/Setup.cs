9955a839854b01ac9cd2567ac5e50ccf61983026
MikeSW <MikeSW@localhost>
2012-07-17 21:42:07 +0300

    added tests for ServiceBus

---
+++ src/XTests/Infrastructure/ServiceBus/Setup.cs
@@ -0,0 +1,34 @@
+﻿using CavemanTools.Infrastructure;
+
+namespace XTests.Infrastructure.ServiceBus
+{
+    public class Setup
+    {
+         
+    }
+
+    public class MyCommand:ICommand
+    {
+        
+    }
+
+    public class MyChildCommand:MyCommand
+    {
+        
+    }
+
+    public class MyEvent:IEvent
+    {
+        
+    }
+
+    public class MyChildEvent:MyEvent
+    {
+        
+    }
+
+    public class MyError:AbstractErrorMessage
+    {
+        
+    }
+}
