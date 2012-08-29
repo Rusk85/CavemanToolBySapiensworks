94af654b83795bc548b88e1b0eac02ff29bad1bf
MikeSW <MikeSW@localhost>
2012-08-29 17:27:19 +0300

    added Percentage class

---
+++ src/CavemanTools/Percentage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CavemanTools
+{
+    public class Percentage
+    {
+        private decimal _value;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value">Percentage amount</param>
+        public Percentage(decimal value)
+        {
+            Value = value;
+        }
+
+        public decimal Value
+        {
+            get { return _value * 100; }
+            private set { _value = value / 100; }
+        }
+
+        public decimal ApplyTo(decimal amount)
+        {
+            return amount * Value;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString("P");
+        }
+    }
+}
