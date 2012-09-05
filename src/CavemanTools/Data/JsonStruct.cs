d58f48e4383cb054eca58b966b7afe4b5490c391
MikeSW <MikeSW@localhost>
2012-09-05 13:34:02 +0300

    JsonStruct can be used for single message also

--- src/CavemanTools/Data/JsonStruct.cs
+++ src/CavemanTools/Data/JsonStruct.cs
@@ -5,11 +5,30 @@ namespace CavemanTools.Data
     public class JsonStruct
     {
         public string Status { get; set; }
-        public IDictionary<string, object> Data { get; private set; }
+        public Dictionary<string, object> Data { get; private set; }
+        public string Message { get; set; }
+
+        public JsonStruct(string message,JsonStatus status = JsonStatus.Ok):this(status)
+        {
+            Message = message;
+            Data["Message"] = message;
+        }
         public JsonStruct(JsonStatus status=JsonStatus.Ok)
         {
             Status = status.ToString();
             Data= new Dictionary<string, object>();
         }
     }
+
+    //public class JsonMessageStruct
+    //{
+    //    public string Message { get; set; }
+    //    public string Status { get; set; }
+
+    //    public JsonMessageStruct(JsonStatus status = JsonStatus.Ok, string message = "")
+    //    {
+    //        Message = message;
+    //        Status = status.ToString();
+    //    }
+    //}
 }
