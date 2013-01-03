1acf95b6fc2e10bce0c8b1f1be6d281041753c25
MikeSW <MikeSW@localhost>
2013-01-03 15:24:56 +0200

    tests for expression extension methods

---
+++ src/XTests/Extensions/ExpressionExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq.Expressions;
+using Xunit;
+using System;
+using System.Diagnostics;
+
+namespace XTests.Extensions
+{
+    
+    public class Test
+    {
+        public void Method(int id)
+        {
+            
+        }
+
+        public int Bla()
+        {
+            return 0;
+        }
+
+        public Test Id { get; set; }
+    }
+    
+    public class ExpressionExtensionsTests
+    {
+        private Stopwatch _t = new Stopwatch();
+        private Type _tp;
+
+        public ExpressionExtensionsTests()
+        {
+            _tp = typeof (Test);
+        }
+
+        [Fact]
+        public void get_method_info_from_expression_when_returning_void()
+        {
+            var mi = ExpressionExtensions.GetMethodInfo<Test>(t => t.Method(2));
+            Assert.Equal(_tp.GetMethod("Method"),mi);
+            ExpressionExtensions.GetPropertyInfo<Test>(t => t.Id);
+            ExpressionExtensions.GetPropertyInfo<Test>(t => t.Id.Id);
+        }
+    
+        [Fact]
+        public void get_method_info_from_expression_when_returning_something()
+        {
+            var mi = ExpressionExtensions.GetMethodInfo<Test>(t => t.Bla());
+            Assert.Equal(_tp.GetMethod("Bla"),mi);
+        }
+
+        protected void Write(string format, params object[] param)
+        {
+            Console.WriteLine(format, param);
+        }
+    }
+}
