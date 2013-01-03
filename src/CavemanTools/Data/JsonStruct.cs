3c35da60176d12f82484f9320df639302f345916
MikeSW <MikeSW@localhost>
2013-01-03 15:57:00 +0200

    some refactoring

--- src/CavemanTools/Data/JsonStruct.cs
+++ src/CavemanTools/Data/JsonStruct.cs
@@ -1,34 +1,35 @@
 using System.Collections.Generic;
+using System;
 
 namespace CavemanTools.Data
 {
     public class JsonStruct
     {
+        public const string MessageKey = "Message";
         public string Status { get; set; }
         public Dictionary<string, object> Data { get; private set; }
-        public string Message { get; set; }
+      
 
+        public object this[string key]
+        {
+            get { return Data[key]; }
+            set { Data[key] = value; }
+        }
+
+        /// <summary>
+        /// Sets the status and inserts a message.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="status"></param>
         public JsonStruct(string message,JsonStatus status = JsonStatus.Ok):this(status)
         {
-            Message = message;
-            Data["Message"] = message;
+            this[MessageKey] = message;
         }
         public JsonStruct(JsonStatus status=JsonStatus.Ok)
         {
             Status = status.ToString();
-            Data= new Dictionary<string, object>();
+            Data= new Dictionary<string, object>();            
         }
     }
 
-    //public class JsonMessageStruct
-    //{
-    //    public string Message { get; set; }
-    //    public string Status { get; set; }
-
-    //    public JsonMessageStruct(JsonStatus status = JsonStatus.Ok, string message = "")
-    //    {
-    //        Message = message;
-    //        Status = status.ToString();
-    //    }
-    //}
 }
