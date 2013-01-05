e5d43926439faa46071337ddf2b51ef482b768fa
MikeSW <MikeSW@localhost>
2013-01-05 16:17:04 +0200

    updated tests

---
+++ src/XTests/Extensions/TypeExtensiont.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using FluentAssertions;
+
+namespace XTests.Extensions
+{
+    public class TypeExtensiont
+    {
+        private Stopwatch _t = new Stopwatch();
+
+        public TypeExtensiont()
+        {
+
+        }
+
+        [Fact]
+        public void user_defined_reference_types()
+        {
+            var objects = new object[] {2,Guid.Empty,new byte[0], EnvironmentVariableTarget.Process,"string"};
+            
+            objects.ForEach(o=>o.GetType().IsUserDefinedClass().Should().BeFalse("{0}".Format(o)));
+
+            typeof (int?).IsUserDefinedClass().Should().BeFalse();
+
+            this.GetType().IsUserDefinedClass().Should().BeTrue();
+        }
+
+        protected void Write(string format, params object[] param)
+        {
+            Console.WriteLine(format, param);
+        }
+    }
+}
