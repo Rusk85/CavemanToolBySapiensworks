b15b604a6897a67e3740dd93654ffb569d8ff715
MikeSW <MikeSW@localhost>
2013-01-05 16:13:40 +0200

    overhauled SmartController functionality, no more inheritance! added TransferResult, TransferToRouteResult, TransferToActionResult

---
+++ src/CavemanTools.MVC/Controllers/SmartControllerAttribute.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Reflection;
+using System.Web;
+
+namespace CavemanTools.Mvc.Controllers
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SmartControllerAttribute:ActionFilterAttribute
+    {
+        SmartActionAttribute _action=new SmartActionAttribute();
+        
+        /// <summary>
+        /// Gets or sets the policy to use when a model validation fails.
+        /// If empty, it will default to return a view with the same name as the action.
+        /// The policy can be overriden at action level using <see cref="SmartActionAttribute"/>
+        /// </summary>
+        public Type ValidationFailedPolicy
+        {
+            get { return _action.ValidationFailedPolicy; }
+            set
+            {
+                if (value!=null) _action.ValidationFailedPolicy = value;
+            }
+        }
+
+        private const string MustHandleKey = "_smart-controller-handled";
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            filterContext.HttpContext.Items.Remove(MustHandleKey);
+            if (!filterContext.ActionDescriptor.HasCustomAttribute<SmartActionAttribute>())
+            {
+                _action.OnActionExecuting(filterContext);
+                filterContext.HttpContext.Set(MustHandleKey,true);
+            }
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {          
+            if (filterContext.Canceled) return;
+            var handle = filterContext.HttpContext.Get(MustHandleKey, false);
+            if (handle)
+            {
+                _action.OnActionExecuted(filterContext);
+            }
+        }
+
+        /// <summary>
+        /// Must be registered as generic
+        /// </summary>
+        public static Type[] TypesForDIContainer = new[] {SmartActionAttribute.DefaultPolicy};
+    }
+}
