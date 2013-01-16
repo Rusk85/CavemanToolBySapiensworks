f9079a8c38d8e2e3fb5474b08fd81f204a979a25
MikeSW <MikeSW@localhost>
2013-01-16 16:53:13 +0200

    added LinkedItems class

---
+++ src/CavemanTools/Model/LinkedItems.cs
@@ -0,0 +1,14 @@
+﻿namespace CavemanTools.Model
+{
+    public class LinkedItems<T, V>
+    {
+        public T Item1 { get; set; }
+        public V Item2 { get; set; }
+
+        public LinkedItems(T item1, V item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+    }
+}
