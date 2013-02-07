155ff873216a738086de2383b76ca76f3b36b194
MikeSW <MikeSW@localhost>
2013-02-07 17:19:06 +0200

    added helpers for working with Expression

--- src/CavemanTools/Properties/AssemblyInfo.cs
+++ src/CavemanTools/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.6.2")]
-[assembly: AssemblyFileVersionAttribute("1.6.2")]
+[assembly: AssemblyVersion("1.7.0")]
+[assembly: AssemblyFileVersionAttribute("1.7.0")]
 [assembly: InternalsVisibleTo("XTests")]
 
