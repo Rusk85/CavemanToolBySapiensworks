4cc7abeffbc194fa67425158695b8ffc953a73da
MikeSW <MikeSW@localhost>
2013-07-05 15:52:52 +0300

    value is protected field change ToString() to include type name

--- src/CavemanTools/Model/ValueObjects/AbstractValueObject.cs
+++ src/CavemanTools/Model/ValueObjects/AbstractValueObject.cs
@@ -4,20 +4,25 @@ namespace CavemanTools.Model.ValueObjects
 {
     public abstract class AbstractValueObject<T>
     {
+        protected T _value;
+
         public AbstractValueObject(T value)
         {
             if (!Validate(value)) throw new ArgumentException();
-            Value = value;
+            _value = value;
         }
 
         protected abstract bool Validate(T value);
 
-        public T Value { get; private set; }
+        public T Value
+        {
+            get { return _value; }        
+        }
 
-   
+
         public override string ToString()
         {
-            return Value.ToString();
+            return "[{0}]{1}".ToFormat(GetType().Name,_value);
         }
      }
 }
