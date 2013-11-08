fdfbc798432c174842b8f5cba20b11f788a09ee7
MikeSW <MikeSW@localhost>
2013-11-08 15:28:24 +0200

    removed obsolete stuff: - Rights based authorization - Custom authentication

--- src/CavemanTools.Web/Security/AccessRules/IValidateCredentials.cs
+++
@@ -1,9 +0,0 @@
-﻿using CavemanTools.Web.Authentication;
-
-namespace CavemanTools.Web.Security.AccessRules
-{
-    public interface IValidateCredentials
-    {
-        bool HasValidCredentials(IUserRightsContext user);
-    }
-}
