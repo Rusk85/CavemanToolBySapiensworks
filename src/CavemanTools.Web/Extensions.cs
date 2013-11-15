c3ec2f94fa93d77885eb27f16494c898b2c16c52
MikeSW <MikeSW@localhost>
2013-11-15 14:51:53 +0200

    restructured CavemanTools.Mvc added method ToWebSiteRelativePath from type name

---
+++ src/CavemanTools.Web/Extensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+
+namespace System.Web
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// MyDll.Namespace.MyType -> ~/MyDll/Namespace/MyType
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="excludeName">Exclude assembly name from namespace</param>
+        /// <param name="removeControllerSuffix">If the type is a mvc controller</param>
+        /// <returns></returns>
+        public static string ToWebsiteRelativePath(this Type type,Assembly excludeName=null,bool removeControllerSuffix=true)
+        {
+            //var nspace=type.Namespace;
+            //if (excludeName != null)
+            //{
+            //    var asmName = excludeName.GetName().Name;
+            //    nspace = nspace.Substring(asmName.Length + 1);
+            //}
+
+            var nspace = Urlize(type.Namespace, excludeName);
+            var result = "~/" + nspace+"/";
+            if (removeControllerSuffix)
+            {
+                result += type.Name.Substring(0, type.Name.Length - 10);
+            }
+            else
+            {
+                result += type.Name;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Assembly.Namespace.Hello.WorldType -> [Assembly/]Namespace/Hello
+        /// </summary>
+        /// <param name="namespace"></param>
+        /// <param name="excludeAssemblyName"></param>
+        /// <returns></returns>
+        public static string Urlize(this string @namespace, Assembly excludeAssemblyName =null)
+        {
+            var nspace = @namespace;
+            if (excludeAssemblyName != null)
+            {
+                var asmName = excludeAssemblyName.GetName().Name;
+                nspace = nspace.Substring(asmName.Length + 1);
+            }
+            return nspace.Replace('.', '/');
+        }
+    }
+}
