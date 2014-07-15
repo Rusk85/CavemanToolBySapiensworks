987b24cd511189553cf84d80eea1f44a6224a307
MikeSw <MikeSw@localhost>
2014-07-15 12:55:58 -0400

    added micro messaging support

--- src/CavemanTools/Infrastructure/ICacheData.cs
+++ src/CavemanTools/Infrastructure/ICacheData.cs
@@ -3,22 +3,6 @@ using System.Runtime.Caching;
 
 namespace CavemanTools.Infrastructure
 {
-    public interface IIGetCacheItem
-    {
-        bool Contains(string key);
-        object Get(string key,DateTimeOffset? newExpiration=null);
-
-        /// <summary>
-        /// Gets typed object from cache or the supplied default value if the value doesn't exist.
-        /// Optionally set a new expiration date.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="key"></param>
-        /// <param name="defaultValue">Value to return if the object doesn't exist in cache</param>
-        /// <returns></returns>
-        T Get<T>(string key,T defaultValue=default(T),DateTimeOffset? newExpiration=null);
-    }
-
     public interface ICacheData : IIGetCacheItem
     {
         /// <summary>
