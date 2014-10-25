39a4b027efd8247b848240e02d063c7b6e8f1735
Mihai Mogosanu <mike@sapiensworks.com>
2014-10-25 18:24:04 -0400

    added task based thread Sleep method

--- src/CavemanTools/Model/Persistence/MemoryCollection.cs
+++ src/CavemanTools/Model/Persistence/MemoryCollection.cs
@@ -10,6 +10,7 @@ namespace CavemanTools.Model.Persistence
         {
             public Guid Id { get; set; }
             public T Value { get; set; }
+            public long Timestamp { get; set; }
             public int Version { get; set; }
 
             public VersionedItem(T value)
@@ -17,12 +18,23 @@ namespace CavemanTools.Model.Persistence
                 Id = value.Id;
                 Value = value;
                 Version = 1;
+                Timestamp = DateTime.UtcNow.Ticks;
             }
         }
 
         public IEnumerable<T> GetItems<T>() where T : IEntity
         {
             return this.OfType<VersionedItem<T>>().Select(d => d.Value).Union(this.OfType<T>());
+        }
+
+        public void Remove<T>(Func<T, bool> match) where T:class
+        {
+            this.RemoveAll(o =>
+            {
+                 var c = d as T;
+                if (c == null) return false;
+                return match(c);
+            })
         }
     }
 }
