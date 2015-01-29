9730672dfde3172e77bcd0b85a0ed471a3a774d3
Mihai Mogosanu <mike@sapiensworks.com>
2015-01-29 18:12:09 -0500

    added more ext methods

--- src/CavemanTools/Properties/AssemblyInfo.cs
+++ src/CavemanTools/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.20.0.0")]
-[assembly: AssemblyFileVersionAttribute("3.20.0.0")]
+[assembly: AssemblyVersion("3.21.0.0")]
+[assembly: AssemblyFileVersionAttribute("3.21.0.0")]
 [assembly: InternalsVisibleTo("XTests")]
 
